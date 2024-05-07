b8cf5783a546f3486605ff5fdff361d51e0365b3
KennethEvolves <kennethgaeliuitrios02@gmail.com>
2024-05-07 15:08:07 -0600

    Update App.jsx component, Input move

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { ChatView, TopBar } from './components'
+import { ChatView, Input, TopBar } from './components'
 
 const App = () => {
     return (
-        <div className='h-screen flex flex-col justify-center items-center font-display'>
+        <div className='h-screen flex flex-col justify-end items-center font-display'>
             <TopBar />
             <ChatView />
+            <Input />
         </div>
     )
 }
--- src/components/ChatView.jsx
+++ src/components/ChatView.jsx
@@ -3,7 +3,6 @@ import { Input } from './Input'
 export const ChatView = () => {
     return (
         <div className='w-full h-screen flex flex-col-reverse justify-start items-center'>
-            <Input />
         </div>
     )
 }
