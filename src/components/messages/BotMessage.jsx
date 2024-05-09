afb21a39f0b3fd85a8fbcc5de9358c524dc7585c
KennethEvolves <kennethgaeliuitrios02@gmail.com>
2024-05-08 20:09:05 -0600

    chore: Update message styling and layout in BotMessage and UserMessage components

--- src/components/messages/BotMessage.jsx
+++ src/components/messages/BotMessage.jsx
@@ -1,7 +1,8 @@
 export const BotMessage = () => {
     return (
-        <article className='bg-gray-p text-gray-s font-semibold p-5 rounded-3xl rounded-bl-none self-start'>
-            Hello, I'm fine, How can i help you?
+        <article className='bg-gray-p max-w-80 text-gray-s text-justify font-semibold p-5 rounded-3xl rounded-bl-none self-start md:max-w-md'>
+            Hello, I'm fine, How can i help you? Labore ipsum quis laborum occaecat mollit duis qui aute do cupidatat.
+            Ex fugiat nulla veniam adipisicing veniam aliquip sunt fugiat reprehenderit non laboris.
         </article>
     )
 }
--- src/components/messages/UserMessage.jsx
+++ src/components/messages/UserMessage.jsx
@@ -1,7 +1,7 @@
-export const UserMessage = () => {
+export const UserMessage = ({ message }) => {
     return (
-        <article className='bg-primary text-white font-medium p-5 rounded-3xl rounded-tr-none self-end'>
-            Hello ChatGPT, how are you today?
+        <article className='bg-primary max-w-80 text-white text-justify font-medium p-5 rounded-3xl rounded-tr-none self-end md:max-w-md'>
+            {message}
         </article>
     )
 }
