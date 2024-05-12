45c6cb607ec57ff336b932ef94af91f7d6e4cb7c
KennethEvolves <kennethgaeliuitrios02@gmail.com>
2024-05-12 17:29:51 -0600

    refactor: Add scroll functionality to ChatView component

--- src/components/ChatView.jsx
+++ src/components/ChatView.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { UserMessage } from './messages'
 
 export const ChatView = ({ messages }) => {
 
+    const messagesEndRef = useRef(null)
+
+    useEffect(() => {
+        messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' })
+    }, [messages])
+
     return (
-        <div className='w-full h-screen py-6 px-4 flex flex-col justify-strt items-center gap-8 
+        <div className='w-full h-screen py-6 px-4 flex flex-col items-center gap-8 
         overflow-y-auto snap-y max-h-lvh 2xl:px-[550px] xl:px-[250px] md:px-[70px]'>
             {
                 messages.map((message, index) => {
@@ -15,6 +21,7 @@ export const ChatView = ({ messages }) => {
                     )
                 })
             }
+            <div ref={messagesEndRef} />
         </div>
     )
 }
